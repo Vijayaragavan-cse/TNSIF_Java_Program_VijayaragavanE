a0e17f090154129e9ca6d63f125deed49517cca6
JET12 <JET12@DESKTOP-VUU9VIE>
2025-07-15 19:57:52 +0530

    Initial Commit

---
+++ Tnsif_Javaprogram/src/mavenrepo/App.java
@@ -0,0 +1,20 @@
+package mavenrepo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+    	
+    	
+    	BangaloreFlight obj = (BangaloreFlight) context.getBean("welcome");
+    	
+    	
+    	System.out.println(obj.getMessage());
+    	
+    }
+}
---
+++ Tnsif_Javaprogram/src/mavenrepo/BangaloreFlight.java
@@ -0,0 +1,18 @@
+package mavenrepo;
+
+public class BangaloreFlight {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+	
+}
